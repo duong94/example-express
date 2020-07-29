a760db9fc0d492492bde29837a2aaa8891693c87
Admin <241538nxdd@gmail.com>
2020-07-29 18:22:01 +0700

    Bai 14 cookie

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -19,6 +19,7 @@ module.exports.search = function(req, res) {
 };
 
 module.exports.create = function(req, res){
+    console.log(req.cookies);
     res.render('users/create');
 };
 
@@ -34,9 +35,7 @@ module.exports.get = function(req, res) {
 
 module.exports.postCreate = function(req, res) {
     req.body.id = shortid.generate();
-
-    console.log(res.locals);
-
+    
     db.get('users').push(req.body).write();
     res.redirect('/users');
 };
--- validate/user.validate.js
+++ validate/user.validate.js
@@ -16,6 +16,5 @@ module.exports.postCreate = function(req, res, next) {
         });
        return; 
     }
-    res.locals.success = true;
     next();
 };
